d0e4597c929018e49090a231c264478c092c2445
Linda Portillo <linda.portillov@gmail.com>
2022-08-20 12:50:05 -0500

    Inicio de la prueba de repositorio en GIT para App 1249r

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main (String[] args){
         System.out.println("Hola");
+        System.out.println("Hola 1249r");
     }
 //https://github.com/Aprendices-Fuera-de-Linea/Aplicativo.git
 }
