f5b96ef30ec1134bd9e5736d2d62b883a46a7f88
Linda Portillo <linda.portillov@gmail.com>
2022-08-20 13:34:45 -0500

    Inicio de la prueba de repositorio en GIT para App 1332r

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main (String[] args){
         System.out.println("Hola");
-        System.out.println("Hola 1332r");
+        System.out.println("Hola 1332r dia sabado");
     }
 //https://github.com/Aprendices-Fuera-de-Linea/Aplicativo.git
 }
