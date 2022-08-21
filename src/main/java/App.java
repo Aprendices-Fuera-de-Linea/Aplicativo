a590aaabafc2cb69a8a3b06008b84e3e20dd3bde
dairo93 <dairo93@gmail.com>
2022-08-20 21:08:25 -0500

    Validación Commit y Conexión

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main (String[] args){
         System.out.println("Hola");
         System.out.println("Hola 1328r");
+        System.out.println("Prueba Commit");
     }
 //https://github.com/Aprendices-Fuera-de-Linea/Aplicativo.git
 }
