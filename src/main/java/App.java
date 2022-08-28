81ad40e6694772f3610969437336686938a5d104
MJersson06 <Jersson2805@hotmail.com>
2022-08-28 15:25:54 -0500

    Comit para prueba github

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main (String[] args){
         System.out.println("Hola");
         System.out.println("ultimo cambio para presentacion sprint1");
+        System.out.println("Hola mundo");
     }
 //https://github.com/Aprendices-Fuera-de-Linea/Aplicativo.git
 }
